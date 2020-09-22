ebbd3e5161a502a11f43b625522e318542406263
KYLE DOHERTY <kyledoherty@KYLEs-iMac-Pro.local>
2020-09-22 13:39:38 -0700

    grad all tiles and mosaic in og projection

--- download_polaris_dat.R
+++ download_polaris_dat.R
@@ -6,19 +6,41 @@ library(stringr)
 baseDir <- '~/mpgPostdoc/projects/forbNiche/data/rasters/environemntal/soil/'
 if(!dir.exists(baseDir)){dir.create(baseDir)}
 
-#the following determines the tile to download based on the extent of a template raster extent
+#the following determines the tiles to download based on the extent of a template raster extent
 template <- raster('~/mpgPostdoc/projects/forbNiche/data/rasters/environemntal/env.tif')
-e <- extent(template)
-centroid <- matrix(data = c(mean(e[1],e[2]), mean(e[3],e[4])),  1, 2)
-epsgs <- make_EPSG()
-wgs84 <- epsgs$prj4[which(epsgs$code == '4326')]
+e <- as(extent(template), 'SpatialPolygons')
+proj4string(e) <- crs(template)
+eLatLon <- extent(spTransform(e, "+proj=longlat +datum=WGS84 +no_defs +type=crs"))
 
-sourcePts <- SpatialPoints(coords = centroid, proj4string=crs(template))
-targPoints <- spTransform(sourcePts, wgs84)
-xcoord <- targPoints@coords[1]# Here you could supply your own centroid by another method 
-ycoord <- targPoints@coords[2]
+tileRas <- raster(xmn = floor(eLatLon[1]),
+       xmx = ceiling(eLatLon[2]),
+       ymn = floor(eLatLon[3]), 
+       ymx = ceiling(eLatLon[4]), 
+       res = 1)
 
-tileInd <- paste0('lat', floor(ycoord), ceiling(ycoord), '_lon', floor(xcoord), ceiling(xcoord), '.tif')
+tileCentroids <- rasterToPoints(tileRas)
+
+tileInds <- character(nrow(tileCentroids))
+
+for(i in seq_along(tileInds)) {
+  xcoord <- tileCentroids[i,1]
+  ycoord <- tileCentroids[i,2]
+  tileInds[i] <-
+    paste0(
+      'lat',
+      floor(ycoord),
+      ceiling(ycoord),
+      '_lon',
+      floor(xcoord),
+      ceiling(xcoord),
+      '.tif'
+    )
+}
+
+#Or just provide a centroid 
+# xcoord <- -114.5
+# ycoord <- 46.5
+# tileInds <- paste0(lat', floor(ycoord), ceiling(ycoord), '_lon', floor(xcoord), ceiling(xcoord), '.tif')
 
 serverBase <- 'http://hydrology.cee.duke.edu/POLARIS/PROPERTIES/v1.0/'
 
@@ -43,13 +65,26 @@ depths <- c('0_5/', #all the depth ranges
             '60_100/',
             '100_200/'
             )
+
 sumStat <- '/mean/' #also mode, p5, p50, p95 (p = percentile)
 
-for(var in varList){
-  for(d in depths){
-   fsource <- paste0(serverBase, var, sumStat, d, tileInd) 
-   dest <- paste0(baseDir, var, '_', str_remove(d, '/'), '.tif')
-   download.file(url = fsource, destfile = dest)
+tdir <- tempdir()
+
+for(var in varList) {
+  for (d in depths) {
+    for (ind in tileInds) {
+      fsource <- paste0(serverBase, var, sumStat, d, ind)
+      tdest <- paste0(tdir, '/', ind)
+      download.file(url = fsource, destfile = tdest)
+      rasProc <- raster(tdest)
+      rasProc <- projectRaster(rasProc, template)
+      rasProc <- crop(rasProc, template)
+      writeRaster(rasProc, tdest, overwrite = TRUE)
+    }
+      toMos <- list.files(tdir, pattern = "*.tif", full.names = TRUE)
+      st <- stack(lapply(FUN = raster, X = toMos))
+      out <- max(st, na.rm = TRUE )
+      writeRaster(out , paste0(baseDir, '/', var, '_', str_remove(d, '/'), '.tif'), overwrite = TRUE)
+      file.remove(toMos)
   }
-  
 }
